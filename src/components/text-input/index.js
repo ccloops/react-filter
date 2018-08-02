54c21cc91ec11a6df4db8d025f2d11da60eddd17
Catherine Looper <loopercat@gmail.com>
2018-08-02 11:55:25 -0700

    added a drop down component and prop types

--- src/components/text-input/index.js
+++ src/components/text-input/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const TextInput = ({ inputValue, handleChange }) => (
   <input 
@@ -8,5 +9,10 @@ const TextInput = ({ inputValue, handleChange }) => (
     onChange={ handleChange }
   />
 );
+
+TextInput.propTypes = {
+  inputValue: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default TextInput;
