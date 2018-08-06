c5ae0b4e6ccb29398e6be977349fcdd9ab31c279
Catherine Looper <loopercat@gmail.com>
2018-08-06 14:53:14 -0700

    adding styled components to the text input

--- src/components/text-input/index.js
+++ src/components/text-input/index.js
@@ -2,10 +2,16 @@ import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-// const InputContainer = 
+const InputContainer = styled.input`
+  height: 3em;
+  width: 15em;
+  border: 2px solid black;
+  font-size: 1em;
+  text-align: center;
+`;
 
 const TextInput = ({ inputValue, handleChange }) => (
-  <input 
+  <InputContainer 
     type='text'
     placeholder='type anything'
     onChange={ handleChange }
