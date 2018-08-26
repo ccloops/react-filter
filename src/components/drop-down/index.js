7735e20e3fb1f8563394e80354b859e44885a6bd
Catherine Looper <loopercat@gmail.com>
2018-08-26 16:13:41 -0700

    drop down list is populating

--- src/components/drop-down/index.js
+++ src/components/drop-down/index.js
@@ -1,2 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
+
+export default class DropDown extends Component {
+  render() {
+    const { dogs, inputValue } = this.props;
+    const results = dogs.filter(dog => dog.startsWith(inputValue));
+    return(
+      <ul>
+        {
+          results.map((item, index) => (
+            <li
+              key={`dog-item-${index}`}
+            >{item}</li>
+          ))
+        }
+      </ul>
+    );
+  }
+}
